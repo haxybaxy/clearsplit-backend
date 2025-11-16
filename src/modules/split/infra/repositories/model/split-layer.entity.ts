a3b122b20b72d78d71c8fa442ca3ae0cc22bb9d7
haxybaxy <zaidsaheb4@gmail.com>
2025-11-16 15:18:51 +0300

    add split layer type value object

--- src/modules/split/infra/repositories/model/split-layer.entity.ts
+++ src/modules/split/infra/repositories/model/split-layer.entity.ts
@@ -1,13 +1,22 @@
 import { DatabaseEntity } from '@base/infra/repositories/entities/typeorm/database.entity';
 import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
+import { SplitLayerType } from '@modules/split/domain/split-layer-type.value-object'
 
-export const SPLIT_LAYER_TABLE_NAME = 'split_plan';
+export const SPLIT_LAYER_TABLE_NAME = 'split_layer';
 @Entity(SPLIT_LAYER_TABLE_NAME)
 export class DBSplitLayer extends DatabaseEntity {
   @Column({ nullable: false })
   order: number;
 
-  @Column({ nullable: false })
+  @Column({
+    type: 'enum',
+    enum: SplitLayerType,
+    nullable: false,
+    default: SplitLayerType.Percentage
+  })
+  type: string;
+
+
   //TODO: type and currency and base
 
 
